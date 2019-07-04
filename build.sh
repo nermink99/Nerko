46526274a5f24e9652a78917f66379a7362898aa
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-07-04 11:13:00 -0300

    hotfix gasp & dsig table issues

--- build.sh
+++ build.sh
@@ -5,12 +5,11 @@ echo "Generating Static fonts"
 mkdir -p fonts/ttf
 fontmake -u src/NerkoOne-Regular.ufo2 -o ttf --output-dir fonts/ttf/
 
-#echo "Post processing"
-#ttfs=$(ls fonts/ttf/*.ttf)
-#for ttf in $ttfs
-#do
-#	gftools fix-dsig -f $ttf;
-#	gftools fix-nonhinting $ttf "$ttf.fix";
-#	mv "$ttf.fix" $ttf;
-#done
-#rm fonts/ttfs/*backup*.ttf
+echo "Post processing"
+ttfs=$(ls fonts/ttf/*.ttf)
+for ttf in $ttfs
+do
+	gftools fix-dsig -f $ttf;
+	gftools fix-nonhinting $ttf "$ttf.fix";
+	mv "$ttf.fix" $ttf;
+done
