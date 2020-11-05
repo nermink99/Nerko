f847a0dee8ee07940a298427000dca95413d3a23
Yanone <post@yanone.de>
2020-11-05 12:05:26 +0100

    Moved build script into src folder, adjusted paths

--- src/build.sh
+++ src/build.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
-mkdir fonts/ttf
-rm fonts/ttf/*.ttf
+rm ../fonts/ttf/*.ttf
 
 set -e
 
 echo "Generating Static fonts"
-mkdir -p fonts/ttf
-fontmake -u src/NerkoOne-Regular.ufo -o ttf --output-dir fonts/ttf/
+mkdir -p ../fonts/ttf
+fontmake -u NerkoOne-Regular.ufo -o ttf --output-dir ../fonts/ttf/
 
 echo "Post processing"
-ttfs=$(ls fonts/ttf/*.ttf)
+ttfs=$(ls ../fonts/ttf/*.ttf)
 for ttf in $ttfs
 do
 	gftools fix-dsig -f $ttf;
 	gftools fix-nonhinting $ttf "$ttf.fix";
 	mv "$ttf.fix" $ttf;
 done
-rm fonts/ttf/*backup*.ttf
+rm ../fonts/ttf/*backup*.ttf
